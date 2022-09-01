bb949487ea3ff81a548b06768568ba8c4177c252
Тимофей Чернов <zuuubada@inbox.ru>
2022-09-01 16:56:20 +0300

    Add project files.

---
+++ Classes/Word.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistinctWordsApp.Classes
+{
+    struct Word
+    {
+        readonly string value = "";
+        private int count = 0;
+
+        public Word(string word)
+        {
+            value = word;
+        }
+
+        public string Value()
+        {
+            return value;
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        public void SetCount(int number)
+        {
+            count = number;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Text;
+using System.Linq;
+using DistinctWordsApp.Classes;
+
+string pathread = $"{Directory.GetCurrentDirectory()}\\Books\\book.txt"; //Путь до файла с текстом
+string pathwrite = $"{Directory.GetCurrentDirectory()}\\Books\\words.txt"; //Путь до файла в котором
+                                                                           //будет находится итоговый,
+                                                                           //отсортированный результат
+
+StringBuilder builder = new StringBuilder(); //обьект, в который будет считываться текстовый файл.Используем
+                                             //именно стрингбилдер, чтобы сэкономить память
+
+using (StreamReader sr = File.OpenText(pathread)) //читаем текстовый файл
+{
+	while (!sr.EndOfStream)
+	{
+       builder.Append(sr.ReadLine());
+    }
+   
+}
+
+string book = builder.ToString(); //преобразует стрингбилдер в строчку, для дальнейшей обработки
+
+char[] splitchars = { 
+    '.', ',', '!', '?', ' ',
+    '1', '2', '3', '4', '5',
+    '6', '7', '8', '9', '0',
+    '(',')' };                  //задаем возможные символы, которые отделяют слова
+string[] words = book.Split(splitchars); //разделяем весь текст на отдельные слова
+
+var res = words.Distinct().Where(w=>w!=""); //создаем массив из отдельных неповторяющихся слов
+
+List<Word> listofwords = new List<Word>();
+
+ foreach (var item in res)      //для каждого уникального слова, из массива, мы создаем
+                                //специальный обьект структуру, которая кроме слова,
+                                //хранит количество употрелений этого слова, и добавляем в список
+{
+    Word word = new Word(item);
+    var wordcounts = words.Where(w => w == item).Count();
+    word.SetCount(wordcounts);
+
+    Console.WriteLine($"{word.Value()}-{word.Count()}");
+    listofwords.Add(word);
+}
+
+var orderbycounts = listofwords.OrderByDescending(w => w.Count()); //сортируем список структур по количеству употреблений
+
+using (StreamWriter sw = new StreamWriter(pathwrite,false)) //записываем итоговый результат в отдельный файл
+{
+    foreach (Word item in orderbycounts)
+    {
+       sw.WriteLine($"{item.Value()} - {item.Count()}");
+    }
+
+    Console.WriteLine("Файл с отсортированными словами успешно создан!");
+    Console.ReadKey();
+}
